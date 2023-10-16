98da8654fc6e9bef0c2f852ff2ed2eaefd9258a3
JL-40 <63485977+JL-40@users.noreply.github.com>
2023-10-15 21:03:22 -0400

    attempt to make panorama into an animation

--- Assets/Scripts/TitleScreen/Panorama.cs
+++ Assets/Scripts/TitleScreen/Panorama.cs
@@ -11,11 +11,28 @@ public class Panorama : MonoBehaviour
     private void Awake()
     {
         mainCamera = Camera.main;
+
+        if (PlayerPrefs.HasKey("CameraRotation_X") && PlayerPrefs.HasKey("CameraRotation_Y") && PlayerPrefs.HasKey("CameraRotation_Z") && PlayerPrefs.HasKey("CameraRotation_W"))
+        {
+            mainCamera.transform.rotation = new Quaternion(PlayerPrefs.GetFloat("CameraRotation_X"), PlayerPrefs.GetFloat("CameraRotation_Y"), PlayerPrefs.GetFloat("CameraRotation_Z"), PlayerPrefs.GetFloat("CameraRotation_W"));
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
         mainCamera.transform.Rotate(0f, 1f * speed * Time.deltaTime, 0f, Space.Self);
+
+        SaveRotation();
+    }
+
+    void SaveRotation()
+    {
+        PlayerPrefs.SetFloat("CameraRotation_X", mainCamera.transform.rotation.x);
+        PlayerPrefs.SetFloat("CameraRotation_Y", mainCamera.transform.rotation.y);
+        PlayerPrefs.SetFloat("CameraRotation_Z", mainCamera.transform.rotation.z);
+        PlayerPrefs.SetFloat("CameraRotation_W", mainCamera.transform.rotation.w);
+
+        PlayerPrefs.Save();
     }
 }
